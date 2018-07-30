77eed179436d3c7a3382d498252927e232827624
Alphonse Mariya <alphonse.mariya@hotmail.com>
2018-07-30 15:35:31 +0200

    Ranger: Add fzf mapping

--- .config/ranger/commands.py
+++ .config/ranger/commands.py
@@ -59,3 +59,63 @@ class my_edit(Command):
         # This is a generic tab-completion function that iterates through the
         # content of the current directory.
         return self._tab_directory_content()
+
+
+# https://github.com/ranger/ranger/wiki/Integrating-File-Search-with-fzf
+# Now, simply bind this function to a key, by adding this to your
+# ~/.config/ranger/rc.conf: map <C-f> fzf_select
+class fzf_select(Command):
+    """:fzf_select
+
+    Find a file using fzf.
+
+    With a prefix argument select only directories.
+
+    See: https://github.com/junegunn/fzf
+    """
+    def execute(self):
+        import subprocess
+        if self.quantifier:
+            # match only directories
+            command="find -L . \( -path '*/\.*' -o -fstype 'dev' -o -fstype 'proc' \) -prune \
+            -o -type d -print 2> /dev/null | sed 1d | cut -b3- | fzf-tmux +m"
+        else:
+            # match files and directories
+            command="find -L . \( -path '*/\.*' -o -fstype 'dev' -o -fstype 'proc' \) -prune \
+            -o -print 2> /dev/null | sed 1d | cut -b3- | fzf-tmux +m"
+        fzf = self.fm.execute_command(command, stdout=subprocess.PIPE)
+        stdout, stderr = fzf.communicate()
+        if fzf.returncode == 0:
+            fzf_file = os.path.abspath(stdout.decode('utf-8').rstrip('\n'))
+            if os.path.isdir(fzf_file):
+                self.fm.cd(fzf_file)
+            else:
+                self.fm.select_file(fzf_file)
+
+
+# https://github.com/ranger/ranger/wiki/Integrating-File-Search-with-fzf
+# Now, simply bind this function to a key, by adding this to your
+# ~/.config/ranger/rc.conf: map <C-g> fzf_locate
+class fzf_locate(Command):
+    """:fzf_locate
+
+    Find a file using fzf.
+
+    With a prefix argument select only directories.
+
+    See: https://github.com/junegunn/fzf
+    """
+    def execute(self):
+        import subprocess
+        if self.quantifier:
+            command="locate home media mnt | fzf-tmux -e -i"
+        else:
+            command="locate home media mnt | fzf-tmux -e -i"
+        fzf = self.fm.execute_command(command, stdout=subprocess.PIPE)
+        stdout, stderr = fzf.communicate()
+        if fzf.returncode == 0:
+            fzf_file = os.path.abspath(stdout.decode('utf-8').rstrip('\n'))
+            if os.path.isdir(fzf_file):
+                self.fm.cd(fzf_file)
+            else:
+                self.fm.select_file(fzf_file)
