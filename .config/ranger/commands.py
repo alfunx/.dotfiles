184740cacbb5c26b3c90d60a8db1da9c68c033e3
Alphonse Mariya <alphonse.mariya@hotmail.com>
2018-12-08 16:48:59 +0100

    Ranger: Add fd, mkcd, cq commands

--- .config/ranger/commands.py
+++ .config/ranger/commands.py
@@ -119,3 +119,123 @@ class fzf_locate(Command):
                 self.fm.cd(fzf_file)
             else:
                 self.fm.select_file(fzf_file)
+
+
+from collections import deque
+fd_deq = deque()
+class fd_search(Command):
+    """:fd_search [-d<depth>] <query>
+
+    Executes "fd -d<depth> <query>" in the current directory and focuses the
+    first match. <depth> defaults to 1, i.e. only the contents of the current
+    directory.
+    """
+
+    def execute(self):
+        import subprocess
+        from ranger.ext.get_executables import get_executables
+        if not 'fd' in get_executables():
+            self.fm.notify("Couldn't find fd on the PATH.", bad=True)
+            return
+        if self.arg(1):
+            if self.arg(1)[:2] == '-d':
+                depth = self.arg(1)
+                target = self.rest(2)
+            else:
+                depth = '-d1'
+                target = self.rest(1)
+        else:
+            self.fm.notify(":fd_search needs a query.", bad=True)
+            return
+
+        # For convenience, change which dict is used as result_sep to change
+        # fd's behavior from splitting results by \0, which allows for newlines
+        # in your filenames to splitting results by \n, which allows for \0 in
+        # filenames.
+        null_sep = {'arg': '-0', 'split': '\0'}
+        nl_sep = {'arg': '', 'split': '\n'}
+        result_sep = null_sep
+
+        process = subprocess.Popen(['fd', result_sep['arg'], depth, target],
+                    universal_newlines=True, stdout=subprocess.PIPE)
+        (search_results, _err) = process.communicate()
+        global fd_deq
+        fd_deq = deque((self.fm.thisdir.path + os.sep + rel for rel in
+            sorted(search_results.split(result_sep['split']), key=str.lower)
+            if rel != ''))
+        if len(fd_deq) > 0:
+            self.fm.select_file(fd_deq[0])
+
+
+class fd_next(Command):
+    """:fd_next
+
+    Selects the next match from the last :fd_search.
+    """
+
+    def execute(self):
+        if len(fd_deq) > 1:
+            fd_deq.rotate(-1) # rotate left
+            self.fm.select_file(fd_deq[0])
+        elif len(fd_deq) == 1:
+            self.fm.select_file(fd_deq[0])
+
+
+class fd_prev(Command):
+    """:fd_prev
+
+    Selects the next match from the last :fd_search.
+    """
+
+    def execute(self):
+        if len(fd_deq) > 1:
+            fd_deq.rotate(1) # rotate right
+            self.fm.select_file(fd_deq[0])
+        elif len(fd_deq) == 1:
+            self.fm.select_file(fd_deq[0])
+
+
+class mkcd(Command):
+    """
+    :mkcd <dirname>
+
+    Creates a directory with the name <dirname> and enters it.
+    """
+
+    def execute(self):
+        from os.path import join, expanduser, lexists
+        from os import makedirs
+        import re
+
+        dirname = join(self.fm.thisdir.path, expanduser(self.rest(1)))
+        if not lexists(dirname):
+            makedirs(dirname)
+
+            match = re.search('^/|^~[^/]*/', dirname)
+            if match:
+                self.fm.cd(match.group(0))
+                dirname = dirname[match.end(0):]
+
+            for m in re.finditer('[^/]+', dirname):
+                s = m.group(0)
+                if s == '..' or (s.startswith('.') and not self.fm.settings['show_hidden']):
+                    self.fm.cd(s)
+                else:
+                    ## We force ranger to load content before calling `scout`.
+                    self.fm.thisdir.load_content(schedule=False)
+                    self.fm.execute_console('scout -ae ^{}$'.format(s))
+        else:
+            self.fm.notify("file/directory exists!", bad=True)
+
+
+class cq(Command):
+    """:cq [error code]
+
+    Quit ranger with error code.
+    """
+
+    def execute(self):
+        if self.arg(1):
+            exit(int(self.arg(1)))
+        else:
+            exit(1)
